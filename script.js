881e76b3aeb25abff715fff8cb873e7462fff2d2
cameron rylands <cprylands@gmail.com>
2024-01-26 01:16:55 -0500

    web script

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleSection(sectionId) {
+    const sections = document.querySelectorAll('section');
+    sections.forEach(section => {
+      if (section.id === sectionId) {
+        section.style.height = '50vh';
+      } else {
+        section.style.height = '0';
+      }
+    });
+  }
