6a7afc7e2df0dff45b9845adb3a6ed995002d1e8
cameron rylands <cprylands@gmail.com>
2024-01-27 17:25:15 -0500

    home and about button styles

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 function toggleSection(sectionId) {
-    const sections = document.querySelectorAll('section');
-    sections.forEach(section => {
-      if (section.id === sectionId) {
-        section.style.height = '50vh';
-      } else {
-        section.style.height = '0';
-      }
-    });
-  }
+  const sections = document.querySelectorAll('section');
+  sections.forEach(section => {
+    if (section.id === sectionId) {
+      section.style.height = '50vh';
+    } else {
+      section.style.height = '0';
+    }
+  });
+}
+
+function toggleSection(sectionId) {
+  var items = document.querySelectorAll('nav ul li a');
+  items.forEach(function (item) {
+    item.classList.remove('active');
+  });
+
+  var clickedItem = document.getElementById(sectionId);
+  clickedItem.classList.add('active');
+}
